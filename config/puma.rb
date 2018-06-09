422e43b3e415af620155299a52d2c25154ab40cc
Avi Moradi <avi.moradi@cellebrite.com>
2018-06-09 23:16:23 +0300

    conf

--- config/puma.rb
+++ config/puma.rb
@@ -1,6 +1,5 @@
-port = ENV['PORT'] || 4000
-environment = ENV['RACK_ENV'] || 'development'
-workers = environment == 'development' ? 1 : Integer(ENV['WEB_CONCURRENCY'] || 4)
+port ENV['PORT'] || 4000
+workers Integer(ENV['WEB_CONCURRENCY'] || 4) if ENV['RACK_ENV'] == 'development' 
 threads_count = Integer(ENV['MAX_THREADS'] || 16)
-threads = threads_count, threads_count
+threads threads_count, threads_count
 preload_app!
